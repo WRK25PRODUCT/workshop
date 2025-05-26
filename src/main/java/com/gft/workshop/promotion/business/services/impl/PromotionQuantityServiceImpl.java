a24e8bb81f3320bcc50f0f91b13194843eb636b1
iongarcia <inac@gft.com>
2025-05-26 16:58:42 +0200

    promotionQuantity impl test, create promotion id not null done

---
+++ src/main/java/com/gft/workshop/promotion/business/services/impl/PromotionQuantityServiceImpl.java
@@ -0,0 +1,38 @@
+package com.gft.workshop.promotion.business.services.impl;
+
+import com.gft.workshop.promotion.business.model.PromotionQuantity;
+import com.gft.workshop.promotion.business.services.PromotionQuantityService;
+import com.gft.workshop.promotion.integration.repositories.PromotionQuantityPLRepository;
+import org.dozer.DozerBeanMapper;
+
+public class PromotionQuantityServiceImpl implements PromotionQuantityService {
+
+    private final PromotionQuantityPLRepository promotionQuantityPLRepository;
+
+    private final DozerBeanMapper mapper;
+
+    public PromotionQuantityServiceImpl(PromotionQuantityPLRepository promotionQuantityPLRepository, DozerBeanMapper mapper) {
+        this.promotionQuantityPLRepository = promotionQuantityPLRepository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public Long createPromotionQuantity(PromotionQuantity promotionQuantity) {
+        return 0L;
+    }
+
+    @Override
+    public PromotionQuantity readPromotionQuantityById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void updatePromotionQuantity(PromotionQuantity promotionQuantity) {
+
+    }
+
+    @Override
+    public void deletePromotionQuantity(Long id) {
+
+    }
+}
