ee484977ba50e1a96c198756ebf74150c55f1230
joanbisbalgft <Joan.Bisbal@gft.com>
2025-05-28 15:16:47 +0200

    Update ProductServiceImplTest.java

--- src/test/java/com/gft/workshop/product/unitTests/services/impl/ProductServiceImplTest.java
+++ src/test/java/com/gft/workshop/product/unitTests/services/impl/ProductServiceImplTest.java
@@ -87,7 +87,7 @@ class ProductServiceImplTest {
     }
 
     @Test
-    @DisplayName("read product by Id should throw BusinessException when not found")
+    @DisplayName("readproductbyId should throw BusinessException when not found")
     void readProductByIdNotFoundTest() {
         when(productPLRepository.findById(99L)).thenReturn(Optional.empty());
 
